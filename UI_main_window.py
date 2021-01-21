864903b931044aaa5598cdbaf6b1fdcfb66f20cf
CHR-onicles <tpandivine48@gmail.com>
2021-01-21 21:53:50 +0000

    added custom widgets

--- UI_main_window.py
+++ UI_main_window.py
@@ -1,9 +1,13 @@
 from PyQt5.QtCore import (QSize, Qt, QTimer, pyqtSignal, pyqtSlot)
 from PyQt5.QtGui import (QFont, QPixmap, QIcon)
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QTabWidget, QComboBox, QCompleter, QLineEdit, QStyle)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QTabWidget, QComboBox, QCompleter,
+                             QLineEdit, QStyle, QVBoxLayout, QFormLayout, QHBoxLayout, QFrame, QGroupBox, QWidget,
+                             QStatusBar)
 import sys
 
-import resources_rc
+# Local imports
+import resources_rc, styles
+from custom_widgets import QHSeparationLine, QVSeparationLine
 
 
 
@@ -16,6 +20,7 @@ class UIMainWindow(QMainWindow):
         self.setWindowIcon(QIcon(':/icon/cat'))
         self.resize(1200, 800)
 
+
         self.UIComponents()
 
     def UIComponents(self):
@@ -23,10 +28,62 @@ class UIMainWindow(QMainWindow):
         self.layouts()
 
     def widgets(self):
-        pass
+
+        self.central_widget = QWidget()
+
+        # BIG TITLE ----------------------------------------------------------------------------------------
+        self.lbl_title = QLabel('MEDICAL BILLS 2021')
+        self.lbl_title.setAlignment(Qt.AlignHCenter)
+
+        # STATUS BAR ---------------------------------------------------------------------------------------
+        self.statusBar().showMessage('Welcome, this is the status bar...')
+
+        # TABS ---------------------------------------------------------------------------------------------
+        self.tabs = QTabWidget()
+        self.tab_1 = QWidget()
+        self.tab_2 = QWidget()
+        self.tab_3 = QWidget()
+        self.tabs.addTab(self.tab_1, 'RECEIPT ENTRY')  # may change later
+        self.tabs.addTab(self.tab_2, 'Tab 2')
+        self.tabs.addTab(self.tab_3, 'Tab 3')
+
+        # TAB 1 WIDGETS ------------------------------------------------------------------------------------
+        self.lbl_month = QLabel('Month')
+        self.combo_months = QComboBox()
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                  'October', 'November', 'December']
+
+
 
     def layouts(self):
-        pass
+        # MAIN WINDOW LAYOUT (CENTRAL WIDGET) --------------------------------------------------------------
+        self.central_layout = QVBoxLayout()
+        self.central_widget.setLayout(self.central_layout)
+        self.setCentralWidget(self.central_widget)
+        self.central_layout.setContentsMargins(0, 10, 0, 0)
+        self.central_layout.addWidget(self.lbl_title)
+        self.central_layout.addWidget(self.tabs)
+
+        # TAB 1 LAYOUTS ------------------------------------------------------------------------------------
+        self.tab1_main_layout = QVBoxLayout()
+        self.tab1_quick_search_layout = QHBoxLayout()
+        self.tab1_month_layout = QHBoxLayout()
+        self.tab1_entry_and_details_layout = QHBoxLayout()
+
+
+        # Adding Widgets to TAB 1 Layout -------------------------------------------------------------------
+        self.tab1_main_layout.addLayout(self.tab1_quick_search_layout)
+        self.tab1_main_layout.addLayout(self.tab1_month_layout)
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ custom_widgets.py
@@ -0,0 +1,45 @@
+# ------------------------------------------------------------------------------
+#
+# AUTHOR: CHR-onicles (GitHub)
+# PROJECT MADE WITH: PyQt5
+#
+# This project can be used freely for all uses, as long as they maintain the
+# respective credits only in the Python scripts.
+#
+# There are limitations on Qt licenses if you want to use your products
+# commercially, I recommend reading them on the official website:
+# https://doc.qt.io/qtforpython/licenses.html
+#
+# ------------------------------------------------------------------------------
+
+from PyQt5.QtWidgets import QFrame, QSizePolicy, QWidget
+
+
+
+
+class QHSeparationLine(QFrame):
+    """
+      Custom Class to create a horizontal separation line.
+    """
+    def __init__(self):
+        super().__init__()
+        self.setMinimumWidth(1)
+        self.setFixedHeight(1)
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        return
+
+
+class QVSeparationLine(QFrame):
+    """
+    Custom Class to create a vertical separation line.
+    """
+    def __init__(self):
+        super().__init__()
+        self.setFixedWidth(1)
+        self.setMinimumHeight(1)
+        self.setFrameShape(QFrame.VLine)
+        self.setFrameShadow(QFrame.Sunken)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        return
