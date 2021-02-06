3426a93a7f65feceafff3bd86669887c661ada2c
CHR-onicles <tpandivine48@gmail.com>
2021-02-06 17:40:43 +0000

    reformatted author info

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Medical_Bills_Program
- Draft for future Med Bills GUI Program
+# Medical Bills Entry App v1.0.0
+ App to quickly make entries of medical claims/receipts into a pre-formatted Excel database.
+
+---
+
+## Info:
+- Author: Divine Anum
+- Contact: tpandivine48@gmail.com
+- Version: 1.0.0
+- First release: xx-xx-xx
--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'Med Bills App'
+__date__ = '20/01/2021'
+__version__ = '1.0.0'
 __license__ = 'MIT'
-__version__ = '0.1.0'
 __author__ = 'Divine Anum'
 __contact__ = 'tpandivine48@gmail.com'
 __url__ = 'https://github.com/CHR-onicles/Medical_Bills_Program'
-__description__ = 'App to speed up the process of entering medical bills claims/receipts into a preformatted excel database'
+__description__ = 'App to speed up the process of entering medical bills claims/receipts into a preformatted excel database.'
